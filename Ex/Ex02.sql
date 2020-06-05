2316019fc16e47b4bc68504c06e0d5a52aeae336
'iremys' <'iremys@gmail.com'>
2020-06-05 12:55:18 +0900

    test

--- Ex/Ex02.sql
+++ Ex/Ex02.sql
@@ -1,126 +1,220 @@
-/*단일행 함수*/
-select  initcap('aaaaaa')
-from dual;
+select * from employees;
+select * from departments;
+select employee_id, first_name, last_name from employees;
 
 
-/*문자 함수*/
---initcap() 첫글자만 대문자로 변경
-select  email, 
-        initcap(email)
+--사원의 이름(fisrt_name)과 전화번호 입사일 연봉을 출력하세요
+select first_name, phone_number, hire_date, salary from employees;
+
+--사원의 이름(first_name)과 성(last_name) 급여, 전화번호, 이메일, 입사일을 출력하세요
+--컬럼명이 많을 경우 세로로 표기한다.
+select  first_name,  
+        last_name, 
+        salary, 
+        phone_number, 
+        email, 
+        hire_date --마지막 컬럼 콤마(,) 조심
 from employees;
 
-select email, initcap(email), department_id
-from employees
-where department_id = 100;
-
-
---LOWER(컬럼명),  UPPER(컬럼명)
-select lower('aAaAaAaAaA'), upper('aAaAaAaAaA')
-from dual;
-
-select first_name, lower(first_name), upper(first_name)
-from employees
-where department_id = 100;
-
---SUBSTR(컬럼명, 시작위치, 글자수)
-select substr('abcdefg', 3, 2)
-from dual;
-
-select  first_name,
-        substr(first_name, 1, 3),
-        substr(first_name, -3, 2)
-from employees
-where department_id = 100;
-
---LPAD(컬럼명, 자리수, ‘채울문자’),   RPAD(컬럼명, 자리수, ‘채울문자’)
-select  first_name,
-        lpad(first_name, 10, '*'),
-        rpad(first_name, 10, '*')
+/*select 문 별명*/
+select  employee_id as empNo,
+        first_name "E-name",
+        salary as "급여"
 from employees;
 
---REPLACE (컬럼명, 문자1, 문자2)
-select  first_name,
-        replace(first_name, 'a', '*')
-from employees
-where department_id = 100;
-
-select  first_name,
-        replace(first_name, 'a', '*'),
-        substr(first_name, 2, 3),
-        replace(first_name, substr(first_name, 2, 3), '***')
-from employees
-where department_id =100;
-
---trim() ltrim() rtrim()
-select  ltrim('     aaaa       ') || '-----',
-        rtrim('     aaaa       ') || '-----',
-        trim('     aaaa       ') || '-----'
-from dual;
-
-
-/*숫자 함수*/
---ROUND(숫자, 출력을 원하는 자리수) 
-select  round(123.345, 2) as "r2",
-        round(123.956, 0) as "r0",
-        round(126.456, -1) as "r-1"
-from dual;
-
---TRUNC(숫자, 출력을 원하는 자리수) 
-select  trunc(123.349, 2) as "r2",
-        trunc(123.956, 0) as "r0",
-        trunc(123.456, -2) as "r-2"
-from dual;
-
-/*날짜 함수*/
---sysdate
-select sysdate
-from dual;
-
-select sysdate, first_name
+--사원의 이름(fisrt_name)과 전화번호 입사일 급여 로 표시되도록 출력하세요
+select  first_name "이름",
+        phone_number "전화번호",
+        hire_date "입사일",
+        salary "급여"
 from employees;
 
---날짜함수 – MONTH_BETWEEN(d1, d2) 
-select  sysdate, 
-        hire_date,
-        months_between(sysdate, hire_date)
-from employees
-where department_id = 110;
-
-select  LAST_DAY('20/06/06'),
-        LAST_DAY(sysdate)
-from dual;
-
-
-/*변환 함수*/
-
---TO_CHAR(n,fmt)
---왜 결과가 2개일까???
-select  first_name, to_char(9876, '99999')
-from employees
-where department_id = 110;
-
-select  to_char(9876, '99999'),
-        to_char(9876, '099999'),
-        to_char(9876, '$9999'),
-        to_char(9876, '9999.99'),
-        to_char(9876, '99,999')
-from dual;
-
-
---TO_CHAR(n,fmt)
-select  sysdate,
-        to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS')
-from dual;
-
-
---NVL(컬럼명, null일때값), NVL2(컬럼명, null아닐때값, null일때 값)
-select  first_name, 
-        commission_pct,
-        nvl(commission_pct, 0),
-        salary * commission_pct,
-        salary * nvl(commission_pct, 0),
-        nvl2(commission_pct, 1234, 0)
+--사원의 사원번호 이름(first_name) 성(last_name) 급여 전화번호 이메일 입사일로 표시되도록 출력하세요
+select  first_name as "이름",
+        last_name as "성",
+        salary as "급여",
+        phone_number as "전화번호",
+        email as "이메일",
+        hire_date as "입사일"
 from employees;
+
+/*select 문 컬럼 합치기*/
+select  first_name, last_name
+from employees;
+
+select  first_name || last_name
+from employees;
+
+select  first_name || ' ' || last_name
+from employees;
+
+select  first_name || ' hire date is '  || hire_date empInfo
+from employees;
+
+--홑따옴표 표기
+select  first_name || ' ''hire date is'' '  || hire_date empInfo
+from employees;
+
+/*산술 연산자 사용하기*/
+select  first_name, salary
+from employees;
+
+select  first_name, salary*12
+from employees;
+
+select  first_name, salary, salary*12
+from employees;
+
+select  first_name, salary, salary*12, (salary+300)*12, salary/30
+from employees;
+
+--문자열은 산술연산 안됨
+select  job_id*12
+from employees;
+
+
+/*예제*/
+select  first_name || '-' || last_name as "성명",
+        salary as "급여",
+        salary*12 as "연봉",
+        salary*12+500 as "연봉2",
+        phone_number as "전화번호"
+from employees;
+
+
+/*select문 where 절*/
+--연봉이 15000 이상인 사원들의 이름과 연봉을 출력하세요
+select  first_name, salary
+from employees
+where salary >= 15000;
+
+--이름이 Lex인 직원의 연봉을 출력하세요
+select  first_name
+from employees
+where first_name = 'Lex';
+
+--07/01/01 일 이후에 입사한 사원들의 이름과 입사일을 출력하세요
+select  first_name, hire_date
+from employees
+where hire_date >= '07-01-01';
+
+
+/*select문 where 절 조건 여러개*/
+--연봉이 14000 이상 17000이하인 사원의 이름과 연봉을 구하시오
+select  first_name, salary
+from employees
+where salary >= 14000
+and salary <= 17000;
+
+--연봉이 14000 이하이거나 17000 이상인 사원의 이름과 연봉을 출력하세요
+select  first_name, salary
+from employees
+where salary <= 14000
+or salary >= 17000;
+
+--입사일이 04/01/01 에서 05/12/31 사이의 사원의 이름과 입사일을 출력하세요
+select  first_name, hire_date
+from employees
+where hire_date >= '04/01/01'
+and  hire_date <= '05/12/31';
+
+
+--연봉이 14000 이상 17000이하인 사원의 이름과 연봉을 구하시오
+--(between and 사용)
+select  first_name, salary
+from employees
+where salary between 14000 and 17000;
+
+--(in 사용)
+select  first_name, last_name, salary
+from employees
+where first_name in ('Neena', 'Lex', 'John');
+
+select  first_name, last_name, salary
+from employees
+where first_name = 'Neena'
+or first_name = 'Lex' 
+or first_name = 'John';
+
+--연봉이 2100, 3100, 4100, 5100 인 사원의 이름과 연봉을 구하시오
+select  first_name, salary
+from employees
+where salary in (2100, 3100, 4100, 5100);
+
+select  first_name, salary
+from employees
+where salary = 2100
+or salary = 3100 
+or salary = 4100
+or salary = 5100;
+
+--(like 사용)
+select  first_name, last_name, salary
+from employees
+where first_name like '_L__';
+
+--이름에 am 을 포함한 사원의 이름과 연봉을 출력하세요
+select  first_name, salary
+from    employees
+where   first_name like '%am%';
+
+--이름의 두번째 글자가 a 인 사원의 이름과 연봉을 출력하세요
+select  first_name, salary
+from    employees
+where   first_name like '_a%';
+
+--이름의 네번째 글자가 a 인 사원의 이름을 출력하세요
+select  first_name, salary
+from    employees
+where   first_name like '___a%';
+
+--이름이 4글자인 사원중 끝에서 두번째 글자가 a인 사원의 이름을 출력하세요
+select  first_name, salary
+from    employees
+where   first_name like '__a_';
+
+/* null */
+select first_name, salary, commission_pct, salary*commission_pct
+from employees
+where salary between 13000 and 15000;
+
+select first_name, salary, commission_pct
+from employees
+where commission_pct = null;
+
+--커미션비율이 있는 사원의 이름과 연봉 커미션비율을 출력하세요
+select  first_name, salary, commission_pct
+from employees
+where commission_pct is not null;
+
+--담당매니저가 없고 커미션비율이 없는 직원의 이름을 출력하세요
+select  first_name
+from employees
+where manager_id is null
+and commission_pct is null;
+
+/*order by*/
+select first_name, salary
+from employees
+where salary >= 9000
+order by salary desc, first_name desc;
+
+--부서번호를 오름차순으로 정렬하고 부서번호, 급여, 이름을 출력하세요
+select salary, first_name
+from employees
+order by department_id asc;
+
+--급여가 1000 이상인 직원의 이름 급여를 급여가 큰직원부터 출력하세요
+select first_name, salary
+from employees
+where salary >= 1000
+order by salary desc;
+
+--부서번호를 오름차순으로 정렬하고 부서번호가 같으면 급여가 높은 사람부터 부서번호 급여 이름을 출력하세요  
+select department_id, salary, first_name
+from employees
+order by department_id asc, salary desc;
+
 
 
 
