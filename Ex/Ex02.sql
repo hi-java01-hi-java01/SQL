288435c448e93e6ef10a7ae3d34fb66b4c2ed6ae
'iremys' <'iremys@gmail.com'>
2020-06-05 13:00:21 +0900

    test

--- Ex/Ex02.sql
+++ Ex/Ex02.sql
@@ -10,4 +10,16 @@ select  email,
 from employees;
 
 
+select email, initcap(email), department_id
+from employees
+where department_id = 100;
 
+
+
+--LOWER(컬럼명),  UPPER(컬럼명)
+select lower('aAaAaAaAaA'), upper('aAaAaAaAaA')
+from dual;
+
+select first_name, lower(first_name), upper(first_name)
+from employees
+where department_id = 100;
