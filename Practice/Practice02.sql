d52e2c7aa80e53173bda4c585e9dbb2304b4b7ee
remys <remys@DESKTOP-KCTNTIS>
2020-06-09 15:09:29 +0900

    select문 그룹함수 CASE ~ END 문 풀이

--- Practice/Practice02.sql
+++ Practice/Practice02.sql
@@ -1,146 +1,126 @@
 --집계(통계) SQL 문제입니다.
 
 /*
-문제 1. 
-전체직원의 다음 정보를 조회하세요. 정렬은 입사일(hire_date)의 올림차순(ASC)으로 
-가장 선임부터 출력이 되도록 하세요. 
-이름(first_name last_name),  월급(salary), 전화번호(phone_number), 입사일(hire_date) 
-순서이고 “이름”, “월급”, “전화번호”, “입사일” 로 컬럼이름을 대체해 보세요.
+문제1.
+매니저가 있는 직원은 몇 명입니까? 쿼리문을 작성하세요
 */
-select  first_name || '  ' || last_name as 이름,
-        salary as 월급,
-        phone_number as 전화번호,
-        hire_date as 입사일
+select  count(manager_id) "haveMngCnt"
 from employees
-order by hire_date asc ;
+where manager_id is not null;
 
 
 
 /*
-문제2.
-업무(jobs)별로 업무이름(job_title)과 최고월급(max_salary)을 월급의 내림차순(DESC)로 
-정렬하세요.
+문제2. 
+직원중에 최고임금(salary)과  최저임금을 “최고임금, “최저임금”프로젝션 
+타이틀로 함께 출력해 보세요. 두 임금의 차이는 얼마인가요?  
+“최고임금 – 최저임금”이란 타이틀로 함께 출력해 보세요.
 */
-select  job_title,
-        max_salary
-from jobs
-order by max_salary desc ;
+select max(salary) as "최고임금",
+       min(salary) as "최저임금",
+       max(salary)-min(salary) as "최고임금-최저임금"
+from employees;
 
 
 
 /*
 문제3.
-담당 매니저가 배정되어있으나 커미션비율이 없고, 월급이 3000초과인 직원의 
-이름, 매니저아이디, 커미션 비율, 월급 을 출력하세요.
+마지막으로 신입사원이 들어온 날은 언제 입니까? 다음 형식으로 출력해주세요.
+예) 2014년 07월 10일
 */
-select  first_name, 
-        manager_id, 
-        commission_pct, 
-        salary
-from employees
-where manager_id is not null 
-and commission_pct is null
-and salary > 3000
-order by salary desc ;  --문제에 정렬은 없지만 결과 확인을 위해 추가하였습니다.
-
+select to_char(max(hire_date), 'yyyy"년" mm"월" dd"일"') 
+from employees;
 
 
 /*
 문제4.
-최고월급(max_salary)이 10000 이상인 업무의 이름(job_title)과 
-최고월급(max_salary)을 최고월급의(max_salary) 내림차순(DESC)로 정렬하여 출력하세요.    
+부서별로 평균임금, 최고임금, 최저임금을 부서아이디(department_id)와 함께 출력합니다.
+정렬순서는 부서번호(department_id) 내림차순입니다.
 */
-select  job_title, 
-        max_salary
-from jobs
-where max_salary >= 10000
-order by max_salary desc ;
-
+select department_id, avg(salary), max(salary), min(salary), 
+from employees
+group by department_id
+order by department_id desc ;
 
 
 /*
 문제5.
-월급이 14000 미만 10000 이상인 직원의 이름(first_name), 월급, 커미션퍼센트 를  
-월급순(내림차순) 출력하세오. 단 커미션퍼센트 가 null 이면 0 으로 나타내시오
+업무(job_id)별로 평균임금, 최고임금, 최저임금을 업무아이디(job_id)와 함께 
+출력하고 정렬순서는 최저임금 내림차순, 평균임금 오름차순 순입니다.
+(정렬순서는 최소임금 2500 구간일때 확인해볼 것)
 */
-select  first_name, 
-        salary, 
-        nvl(commission_pct, 0)
-from employees 
-where salary < 14000
-and salary >= 10000 
-order by salary desc;
-
+select job_id, round(avg(salary),0), max(salary), min(salary)
+from employees
+group by job_id
+order by min(salary) desc, avg(salary) asc;
 
 
 /*
 문제6.
-부서번호가 10, 90, 100 인 직원의 이름, 월급, 입사일, 부서번호를 나타내시오
-입사일은 1977-12 와 같이 표시하시오
+가장 오래 근속한 직원의 입사일은 언제인가요? 다음 형식으로 출력해주세요.
+예) 2001-01-13 토요일 
 */
-select  first_name,
-        salary, 
-        to_char(hire_date, 'YYYY-MM' ) hire_date, 
-        department_id
-from employees
-where department_id in ( 10, 90, 100 ) ;
+select to_char(min(hire_date), 'yyyy-mm-dd day')
+from employees;
 
 
 
 /*
 문제7.
-이름(first_name)에 S 또는 s 가 들어가는 직원의 이름, 월급을 나타내시오
+평균임금과 최저임금의 차이가 2000 미만인 부서(department_id), 평균임금, 최저임금 
+그리고 (평균임금 – 최저임금)를 (평균임금 – 최저임금)의 내림차순으로 정렬해서 출력하세요.
 */
-select  first_name, 
-        salary 
+select  department_id, 
+        avg(salary), 
+        min(salary),
+        avg(salary) - min(salary)
 from employees
-where upper(first_name) like '%S%' ;
+group by department_id
+having avg(salary) - min(salary) < 2000
+order by avg(salary) - min(salary) desc;
 
 
 
 /*
 문제8.
-전체 부서를 출력하려고 합니다. 순서는 부서이름이 긴 순서대로 출력해 보세오.
+업무(JOBS)별로 최고임금과 최저임금의 차이를 출력해보세요.
+차이를 확인할 수 있도록 내림차순으로 정렬하세요? 
 */
-select department_name
-from departments
-order by length(department_name) desc ;
-
-
-
-/*
-문제9.
-정확하지 않지만, 지사가 있을 것으로 예상되는 나라들을 나라이름을 대문자로 출력하고
-올림차순(ASC)으로 정렬해 보세오.
-*/
-select upper(country_name) country_name 
-from countries
-order by upper(country_name) asc ;
-
-
-/*
-문제10.
-입사일이 03/12/31 일 이전 입사한 직원의 이름, 월급, 전화 번호, 입사일을 출력하세요
-전화번호는 545-343-3433 과 같은 형태로 출력하시오.
-*/
-select  first_name, 
-        salary,
-        replace(phone_number, '.', '-') phone_number, 
-        hire_date
+select  job_id, 
+        max(salary) - min(salary) as diff_salary
 from employees
-where hire_date <= '03/12/31' ;
+group by job_id
+order by diff_salary desc;
 
 
-
----------------------------------------
 /*
-직원테이블의 이메일 컬럼에는 직원의 이메일주소의 아이디 부분이 대문자로 저장되어 있습니다. 
-직원의 이름 이메일 소문자이메일 주소가 출력되도록 하세요. 
-소문자 이메일주소 예제) ABANDA --> abanda@gmail.com
+문제9
+2005년 이후 입사자중 관리자별로 평균급여 최소급여 최대급여를 알아보려고 한다.
+출력은 관리자별로 평균급여가 5000이상 중에 평균급여 최소급여 최대급여를 출력합니다.
+평균급여의 내림차순으로 정렬하고 평균급여는 소수점 첫째짜리에서 반올림 하여 출력합니다.
 */
+select manager_id, round(avg(salary),0), min(salary), max(salary)
+from employees
+where hire_date > '05/01/01'
+group by manager_id
+having avg(salary) >= 5000
+order by avg(salary) desc;
 
-select 	first_name,
-		email, 
-		lower(email) || '@gmail.com'
-from employees;
 
+/*
+문제10
+아래회사는 보너스 지급을 위해 직원을 입사일 기준으로 나눌려고 합니다. 
+입사일이 02/12/31일 이전이면 '창립맴버, 03년은 '03년입사’, 04년은 ‘04년입사’ 
+이후입사자는 ‘상장이후입사’ optDate 컬럼의 데이터로 출력하세요.
+출력형식은 직원아이디, 급여, optDate, 입사일 이며 정렬은 입사일로 오름차순으로 정렬합니다.
+*/
+SELECT  employee_id, 
+        salary,
+        CASE WHEN hire_date <= '02/12/31' THEN '창립맴버'
+             WHEN hire_date >= '03/01/01' and hire_date <= '03/12/31' THEN '03년'
+             WHEN hire_date >= '04/01/01' and hire_date <= '04/12/31' THEN '04년'
+             ELSE '상장이후'
+        END optDate,
+        hire_date
+FROM employees
+order by hire_date asc;
